f066c8116f536a7c17be801c0fb65e0059ae361f
AleixBBPP <aleixboschperez@gmail.com>
2026-04-13 21:16:30 +0200

    feat: link CV PDF to download button

--- js/config.js
+++ js/config.js
@@ -17,7 +17,7 @@ window.CONFIG = {
         phone: "+34 600 000 000",
         location: "España",
         avatar: "assets/images/avatar.jpg",
-        cvUrl: "#"
+        cvUrl: "assets/cv-aleix-bosch.pdf"
     },
 
     stats: [
